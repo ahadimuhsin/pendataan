1d9e7c38d758c4668a1e3efdc34a3af1d9ffc228
Muhsin Ahadi <muhsin_ahadi@student.uns.ac.id>
2018-01-25 10:36:17 +0700

    Update database

--- pendataan.sql
+++ pendataan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2018 at 09:26 AM
+-- Generation Time: Jan 25, 2018 at 04:35 AM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -38,7 +38,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`username`, `password`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3');
+('admin', '21232f297a57a5a743894a0e4a801fc3'),
+('admin2', 'c84258e9c39059a89ab77d846ddab909');
 
 -- --------------------------------------------------------
 
@@ -47,7 +48,7 @@ INSERT INTO `admin` (`username`, `password`) VALUES
 --
 
 CREATE TABLE `company` (
-  `npwp` int(100) NOT NULL,
+  `npwp` text NOT NULL,
   `no_company` int(100) NOT NULL,
   `company` varchar(100) NOT NULL,
   `parent_company` varchar(100) NOT NULL,
@@ -56,8 +57,8 @@ CREATE TABLE `company` (
   `contact_person` varchar(50) NOT NULL,
   `title` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `phone` int(50) NOT NULL,
-  `mobile` int(50) NOT NULL,
+  `phone` text NOT NULL,
+  `mobile` text NOT NULL,
   `business_consultant` varchar(100) NOT NULL,
   `join_date` date NOT NULL,
   `assignment` varchar(50) NOT NULL
@@ -68,12 +69,13 @@ CREATE TABLE `company` (
 --
 
 INSERT INTO `company` (`npwp`, `no_company`, `company`, `parent_company`, `address`, `city`, `contact_person`, `title`, `email`, `phone`, `mobile`, `business_consultant`, `join_date`, `assignment`) VALUES
-(2345678, 1, 'Huaweiiiii', 'Indosat', 'Jakarta', 'Jakarta', 'Muhammad', 'Bos', 'manager@gmail.com', 3456789, 345678, 'Movita Suci Mutiara', '2018-01-04', 'Project Assignment'),
-(878711111, 2, 'Kaskus', 'Kasku', 'Jakarat', 'Jakarat', 'bhjbsd', 'jhsad', 'jhsdja@gmail.com', 34567890, 987654, 'Mochamad Badriansyah', '2018-01-09', 'Project Assignment'),
-(34343, 3, 'fdsfw', 'frewrw', 'vdasvs', 'ewrwer', 'gegre', 'wertwr', 'werwer@dsada.com', 42323231, 432342, 'Movita Suci Mutiara', '2018-01-11', 'Lead Assignment'),
-(43123, 4, 'lalala', 'dfghjkl;', 'rtyuiop', 'fghjkl;', '34567890', '4567890', 'gvjhk@sdas.com', 56789, 56789, 'Hadi Fatkhul Amin', '2018-01-02', 'Project Assignment'),
-(5345, 5, 'ergr', 'terte', 'gerge', 'erge', 'ererge', 'erge', 'dqwd@sada.com', 56789, 567890, 'Movita Suci Mutiara', '2018-01-04', 'Project Assignment'),
-(12345, 6, 'rerere', 'fefefe', 'sasasa', 'dadada', 'wawaa', 'rarara', 'abang@mail.com', 3535, 35435, 'Sigit Tri Marwanto', '2018-01-19', 'Project Assignment');
+('75.652.621.6-.125', 2, 'Lintas Media', 'Lintasarta', 'Lebak Bulus', 'Jakarta', 'Sigit Tri Marwanto', 'Marketing', 'sdaasd@sada.com', '65645645', '6535454', 'Movita Suci Mutiara', '2018-01-17', 'Replacement'),
+('34.234.234.3-242.352', 3, 'Hmmmm', 'fwefw', 'werwr', 'fwfgwfg', 'rterter', 'Muhsin', 'dada@gmail.com', '+62982-3829-3829', '+62839-2839-2389', 'Shoba Mediwati', '2018-01-08', 'Project Assignment'),
+('34.234', 4, 'Google', 'rewrwe', 'verge', 'erwrr', '23rc2r', 'f wfewfr', 'adasad@dsa.com', '432424', '2343424', 'Hadi Fatkhul Amin', '2018-01-16', 'Project Assignment'),
+('12.345.678.9-098.765', 6, 'SMA 94 Jakarta', 'SMA 94 Jakarta', 'Kalideres', 'Jakarta Barat', 'Nengsih', 'Kepala Sekolah', 'nengsih@lalala.com', '+62672-3672-8723', '+62895-1970-5026', 'Movita Suci Mutiara', '2018-01-09', 'Lead Assignment'),
+('91.287.381.7-218.218', 7, 'SMP 264 Jakarta', 'SMP 264 Jakarta', 'Cengkareng', 'Jakarta', 'Satar', 'Kepsek', 'satar@kakaka.com', '+62238-2938-2938', '+62722-8239-8362', 'Hadi Fatkhul Amin', '2018-01-09', 'Project Assignment'),
+('31.423.423.4-234.242', 8, 'Axis', 'Ahadi', 'rewrwer', 'fwefw', 'rehe', 'rwrwr', 'adad@sds.com', '+62', '+62425-245', 'Mochamad Badrinsyah', '2018-01-10', 'Lead Assignment'),
+('87.923.927.3-.882', 9, 'WWE', 'WWE', 'Seattle', 'New York', 'Vince McMahon', 'CEO', 'vince@wwe.com', '+62232-3287-3217', '+62438-2932-7329', 'Fahmi Ernanto', '2018-01-17', 'Project Assignment');
 
 -- --------------------------------------------------------
 
@@ -103,11 +105,11 @@ CREATE TABLE `jasa` (
 --
 
 INSERT INTO `jasa` (`no_jaringan`, `no_company`, `subservices`, `services`, `product_family`, `contract_start_date`, `contract_end_date`, `spk_number`, `spk_date`, `spk_handover_date`, `otc`, `mrc`, `type`, `keterangan`) VALUES
-(1234, 2, 'Managed Firewall', 'Managed Application', 'Managed Service', '2018-01-16', '2018-01-17', 32332, '2018-01-24', '2018-01-25', 4434, 34234, 'Joining Free', 'Berhasil! Yey'),
-(7890, 3, 'Managed Server', 'Managed Server', 'Managed Service', '2018-01-17', '2018-01-18', 414234234, '2018-01-25', '2018-01-26', 2332, 2323123, 'Joining Free', 'Apaanaaaa'),
-(12345, 1, 'Application Development', 'Application Services', 'SaaS', '2018-01-03', '2018-01-11', 6435453, '2018-01-15', '2018-01-25', 432423, 234324, 'Joining Free', '34324'),
-(432423, 1, 'Managed Mail', 'Managed Application', 'Managed Service', '2018-01-18', '2018-01-27', 4545, '2018-01-26', '2018-01-25', 34234, 235252, 'Joining Free', 'aaaaaaaaa'),
-(55434324, 4, 'Managed Cpanel', 'Managed Application', 'Managed Service', '2018-01-17', '2018-01-26', 53434, '2018-01-25', '2018-01-24', 5534, 342343, 'Joining Free', '3443');
+(1, 2, 'Managed Web', 'Managed Application', 'Managed Service', '2018-01-09', '2018-01-25', 45345345, '2018-01-16', '2018-01-25', 346535435, 2432425, 'Monthly', ''),
+(2, 2, 'IT Solution', 'Professional Services', 'Managed Service', '2018-01-15', '2018-01-26', 436456456, '2018-01-23', '2018-01-26', 363645, 6645645, 'Joining Free', 'Lalala'),
+(3, 3, 'Managed Antivirus', 'Managed Application', 'Managed Services', '2018-01-09', '2018-01-26', 2147483647, '2018-01-16', '2018-01-11', 324324, 324234, 'Monthly', 'Lunas'),
+(4, 4, 'Cloud Server', 'Cloud Server', 'Cloud Services', '2018-01-11', '2018-01-20', 2147483647, '2018-01-17', '2018-01-31', 2532525, 2524535, 'Monthly', 'Hutang'),
+(5, 9, 'Managed SSL', 'Managed Application', 'Managed Services', '2018-01-11', '2018-01-19', 2147483647, '2018-01-24', '2018-01-31', 2524342, 235234234, 'Monthly', '8647134673');
 
 --
 -- Indexes for dumped tables
@@ -140,7 +142,12 @@ ALTER TABLE `jasa`
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `no_company` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `no_company` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `jasa`
+--
+ALTER TABLE `jasa`
+  MODIFY `no_jaringan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
